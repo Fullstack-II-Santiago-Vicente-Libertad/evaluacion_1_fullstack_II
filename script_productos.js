11d3491a4c049b5174e18dd11fb622ad98ba73e3
Vnares94 <vice.menares@duocuc.cl>
2026-09-24 19:01:21 -0300

    Update script_productos.js

--- script_productos.js
+++ script_productos.js
@@ -5,7 +5,13 @@ const listaProductos = [
     { id: 2, nombre: "El obsceno pájaro de la noche", autor: "José Donoso", precio: 10000, imagen: "assets/pajaro.jpg" },
     { id: 3, nombre: "Inés el alma mía", autor: "Isabel Allende", precio: 12000, imagen: "assets/ines del alma mia.jpg" },
     { id: 4, nombre: "Nuestros años verde olivo", autor: "Roberto Ampuero", precio: 14000, imagen: "assets/verde_olivo.jpg" },
-    { id: 5, nombre: "La muerte viene estilando", autor: "Andrés Montero", precio: 9000, imagen: "assets/muerte_estilando.jpg" }
+    { id: 5, nombre: "Bonsai", autor: "Alejandro Zambra", precio: 9000, imagen: "assets/bonsai.jpg" },
+    { id: 6, nombre: "El buzón de las impuras", autor: "Francisca Solar", precio: 20000, imagen: "assets/buzon-de-las-impuras.jpg" },
+    { id: 7, nombre: "La ciudad de las bestias", autor: "Isabel Allende", precio: 13000, imagen: "assets/la-ciudad-de-las-bestias.jpg" },
+    { id: 8, nombre: "La muerte viene estilando", autor: "Andrés Montero", precio: 14500, imagen: "assets/muerte_estilando.jpg" },
+    { id: 9, nombre: "Formas de volver a casa", autor: "Alejandro Zambra", precio: 16000, imagen: "assets/formas-de-volver-a-casa.jpg" },
+    { id: 10, nombre: "Casa de campo", autor: "José Donoso", precio: 11000, imagen: "assets/casa-de-campo.jpg" },
+
 ];
 
 // 2. Función para recorrer el arreglo e inyectarlo en el HTML
@@ -21,7 +27,7 @@ function cargarProductos() {
 
     tarjeta.innerHTML = `
 
-        <img src="${producto.imagen}" width=200 alt="${producto.nombre} ">
+        <img src="${producto.imagen}" width=200px alt="${producto.nombre} ">
         <h3>Título: ${producto.nombre}</h3>
         <p>Autor: ${producto.autor}<p>
         <p class="precio">Precio: $${producto.precio}</p>
@@ -36,4 +42,6 @@ function cargarProductos() {
 // 3. Ejecución al cargar el documento
 
 document.addEventListener("DOMContentLoaded", cargarProductos);
+
+
 
