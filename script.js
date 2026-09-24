9e0182902d334d78ad43eaa916c13925338bdefa
Vnares94 <vice.menares@duocuc.cl>
2026-09-24 17:49:54 -0300

    Update script con validaciones.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ formulario.addEventListener("submit", function(event) {
 
     let nombre = document.getElementById("nombre").value;
     let correo = document.getElementById("correo").value;
-    let password = document.getElementById("password").value;
+    let motivo = document.getElementById("motivo").value;
     let mensaje = document.getElementById("mensaje");
 
     if (nombre == "") {
@@ -15,12 +15,23 @@ formulario.addEventListener("submit", function(event) {
     else if (correo == "") {
         mensaje.textContent = "Debes ingresar tu correo.";
     }
-    else if (password.length < 6) {
-        mensaje.textContent = "La contraseña debe tener al menos 6 caracteres.";
+    else if (nombre.length > 100) {
+        mensaje.textContent = "El nombre excede el máximo de caracteres.";
+    }
+    else if (motivo == "") {
+        mensaje.textContent = "Debes ingresar un motivo.";
+    }
+    else if (correo.length > 100) {
+        mensaje.textContent = "El correo excede el máximo de caracteres.";
+    }
+    else if (!correo.includes("@duocuc.cl") && !correo.includes("@profesor.duoc.cl") && !correo.includes("@gmail.com")) {
+        mensaje.textContent = "Ingresa un correo válido.";
+    }
+    else if (motivo.length > 500) {
+        mensaje.textContent = "El motivo excede el máximo de caracteres.";
     }
     else {
         mensaje.textContent = "Registro realizado correctamente.";
     }
 
 });
-
